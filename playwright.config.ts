c06e1e131bcf6eb414c4d9bb28ef5296a5a8b915
Adam Mitchell <82591699+adad-mitch@users.noreply.github.com>
2023-08-30 15:44:04 +0100

    Add Playwright test port configurability via .env.dev

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,12 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.dev" });
+
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: "npm run build && npm run preview",
-		port: 4173,
+		port: parseInt(process.env.SVELTEKIT_BUILD_PORT || "4173"),
 	},
 	testDir: "tests",
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
