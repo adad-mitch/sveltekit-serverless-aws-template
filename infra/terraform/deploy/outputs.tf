ce589ab79a7482e83722a159e723f754bbfb7e3d
Adam Mitchell <adadmitch@gmail.com>
2025-10-05 23:32:30 +0100

    feat: Provide the option to automatically invalidate CloudFront cache on deploy

--- infra/terraform/deploy/outputs.tf
+++ infra/terraform/deploy/outputs.tf
@@ -2,3 +2,8 @@ output "front_domain" {
   description = "The domain of the application (either the generated AWS CloudFront Distribution URL or a domain you specified successfully)."
   value       = try(module.routing[0], null) != null ? module.routing[0].www_fqdn : module.cdn.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  description = "The CloudFront distribution ID for cache invalidation."
+  value       = module.cdn.id
+}
