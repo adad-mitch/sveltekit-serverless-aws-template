65171ed4465d4f40c410c608b2f4f760d7385a3e
Adam Mitchell <82591699+adad-mitch@users.noreply.github.com>
2025-10-05 23:45:12 +0100

    fix: Decode non-200 responses (#3)

--- infra/lambda-template/lambda.js
+++ infra/lambda-template/lambda.js
@@ -6,8 +6,36 @@ import { handler as svelteHandler } from "./handler.js";
 const app = express();
 app.use(svelteHandler);
 
-const server = createServer(app);
+const binaryMimeTypes = [
+	'application/javascript',
+	'application/json',
+	'application/octet-stream',
+	'application/xml',
+	'font/eot',
+	'font/opentype',
+	'font/otf',
+	'image/jpeg',
+	'image/png',
+	'image/svg+xml',
+	'text/comma-separated-values',
+	'text/css',
+	'text/html',
+	'text/javascript',
+	'text/plain',
+	'text/text',
+	'text/xml'
+]
 
-export const handler = (event, context) => {
-	proxy(server, event, context);
+const server = createServer(app, null, binaryMimeTypes);
+
+export const handler = async (event, context) => {
+	console.log(JSON.stringify(event));
+	const res = await proxy(server, event, context, 'PROMISE').promise;
+
+	// Browsers won't decode 4xx and 5xx responses, so we need to decode them here
+	if (res.statusCode >= 400) {
+		res.body = Buffer.from(res.body, 'base64').toString('utf8');
+	}
+
+	return res;
 };
